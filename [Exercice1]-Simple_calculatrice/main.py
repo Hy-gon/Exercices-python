04d83d01f2280548abca05f2d9a0d04e0a8d2a16
Hy-gon <mickael.lemieux@outlook.com>
2024-03-12 16:47:39 +0100

    first commit

---
+++ [Exercice1]-Simple_calculatrice/main.py
@@ -0,0 +1,24 @@
+nombre_1 = input("Veuillez entrer votre premier nombre positif : ")
+nombre_2 = input("Veuillez entrer votre second nombre positif : ")
+operator = input("Veuillez entrer votre opérateur (+, -, *, /, %) : ")
+
+if nombre_1.isdigit() and nombre_2.isdigit():
+    nombre_1 = int(nombre_1)
+    nombre_2 = int(nombre_2)
+
+    if operator == "+":
+        result = nombre_1 + nombre_2
+    elif operator == "-":
+        result = nombre_1 - nombre_2
+    elif operator == "*":
+        result = nombre_1 * nombre_2
+    elif operator == "/":
+        result = nombre_1 / nombre_2
+    elif operator == "%":
+        result = nombre_1 % nombre_2
+    else:
+        result = "Votre opérateur n'est pas correcte, veuillez reesayer"
+else:
+    result = "Le premier ou second nombre n'est pas un nombre ou n'est pas positif"
+
+print(result)
