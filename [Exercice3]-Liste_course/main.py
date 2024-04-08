9c674faa9e9d7028dc14a06b648c3e6814968f74
Hy-gon <mickael.lemieux@outlook.com>
2024-04-08 19:42:54 +0200

    :arrow_up: changing version to 3.12.2, adding match case

--- [Exercice3]-Liste_course/main.py
+++ [Exercice3]-Liste_course/main.py
@@ -50,9 +50,6 @@ def clear_list(shop_list):
         print("La liste a été vidée de son contenu.")
     else:
         print("la liste est déjà vide.")
-        
-def close_program():
-    sys.exit("A bientôt !")
 
 # Loop program
 def main():
@@ -63,20 +60,22 @@ def main():
         try:
             choix_int = int(choice)  
             
-            if choix_int == 1:
-                element = input("Entrez le nom d'un élément à ajouter à la liste de courses : ")
-                add_element(shop_list, element)
-            elif choix_int == 2:
-                element = input("Entrez le nom d'un élément à retirer de la liste de courses : ")
-                remove_element(shop_list, element)
-            elif choix_int == 3:
-                print_list(shop_list)
-            elif choix_int == 4:
-                clear_list(shop_list)
-            elif choix_int == 5:
-                close_program()
-            else:
-                print("Veuillez choisir une des 5 options suivantes")
+            
+            match choix_int:
+                case 1:
+                    element = input("Entrez le nom d'un élément à ajouter à la liste de courses : ")
+                    add_element(shop_list, element)
+                case 2:
+                    element = input("Entrez le nom d'un élément à retirer de la liste de courses : ")
+                    remove_element(shop_list, element)
+                case 3:
+                    print_list(shop_list)
+                case 4:
+                    clear_list(shop_list)
+                case 5:
+                    sys.exit("A bientôt !")
+                case other:
+                    print("Veuillez choisir une des 5 options suivantes")
                 
             element == ""
             print("_" * 50)
