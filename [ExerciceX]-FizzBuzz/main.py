c11cc32bbf290e0bea04b6b8f23edb6eebde7cc0
Hy-gon <mickael.lemieux@outlook.com>
2024-05-13 14:12:49 +0200

    :arrow_up: Finishing project

--- [ExerciceX]-FizzBuzz/main.py
+++ [ExerciceX]-FizzBuzz/main.py
@@ -4,3 +4,15 @@
 #__Python__ = "3.6.8"
 #__Status__ = "Finished"
 #__Emails__ = "mickael.lemieux@outlook.com"
+
+def main():
+    number = int(input("JEU FIZZBUZZ : Veuillez entrer un nombre : "))
+    output = ""
+    if number % 3 == 0:
+        output += "Fizz"
+    if number % 5 == 0:
+        output += "Buzz"
+    print(output or number)
+    
+if __name__ == "__main__":
+    main()
